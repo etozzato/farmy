828a9dfd57183fae4394b3ca9b2071e53a4df890
Emanuele Tozzato <etozzato@gmail.com>
2017-10-30 12:11:13 +0000

    caching the API access'

--- app/controllers/api/companies_controller.rb
+++ app/controllers/api/companies_controller.rb
@@ -2,7 +2,9 @@ class Api::CompaniesController < ApplicationController
   include ActionView::Helpers::SanitizeHelper
 
   def nif
-    response = HTTParty.get("http://www.nif.pt/?json=1&key=#{ENV['nif_key']}&q=#{params[:id]}")
+    response = Rails.cache.fetch("nif::#{params[:id]}") do
+      HTTParty.get("http://www.nif.pt/?json=1&key=#{ENV['NIF_KEY']}&q=#{params[:id]}")
+    end
     response.parsed_response['records'][params[:id]]['text_activity'] =
       strip_tags(response.parsed_response['records'][params[:id]]['activity'])
     render json: response.parsed_response
